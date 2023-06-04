0a7d241c84c96966f7e0d9cb80de3be02fa432df
Franz Mohr <franz.x.mohr@outlook.com>
2023-06-04 13:39:19 +0200

    Import bvartools methods

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,8 @@
 # Load needed functions from different packages for fast access
 
 #' @importFrom coda thin
+#' @importFrom bvartools add_priors
+#' @importFrom bvartools draw_posterior
 
 # Unload the DLL when the package is unloaded
 .onUnload <- function (libpath) {
