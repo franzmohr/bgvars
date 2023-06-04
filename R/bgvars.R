d4df44ebf1fd6bc01f869b1573c7855c7894905b
Franz Mohr <franz.x.mohr@outlook.com>
2023-06-04 13:47:16 +0200

    Update DESCRIPTION

--- R/bgvars.R
+++ R/bgvars.R
@@ -1,6 +1,6 @@
-#' bgvars: Bayesian Inference for Global Vector Autoregressive Models
+#' bgvars: Bayesian Inference for Global Vector Autoregressive (GVAR) and Global Vector Error Correction (GVEC) Models
 #' 
-#' R/C++ implementation of Bayesian global vector autoregressive models.
+#' R/C++ implementation of Bayesian global vector autoregressive and global vector error correction models.
 #' 
 #' @author Franz X. Mohr
 #' @docType package
