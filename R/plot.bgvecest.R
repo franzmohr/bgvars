aa1ab2670abda0821c078ea5b69db9a04195e530
Franz Mohr <franz.x.mohr@outlook.com>
2023-06-24 11:43:21 +0200

    Country-specific posterior plotting

--- R/plot.bgvecest.R
+++ R/plot.bgvecest.R
@@ -6,35 +6,25 @@
 #' @param ci interval used to calculate credible bands for time-varying parameters.
 #' @param type either \code{"hist"} (default) for histograms, \code{"trace"} for a trace plot,
 #' or \code{"boxplot"} for a boxplot. Only used for parameter draws of constant coefficients.
-#' @param model indicates, for which models plots should be produced. Can be a character
-#' vector of country names or an integer vector of the positions of the elements in
-#' argument \code{x}.
+#' @param ctry character. Name of the element in argument \code{x}, for which posterior draws
+#' should be plotted. If \code{NULL} (default), all submodels are used.
 #' @param ... further graphical parameters.
 #' 
 #' @export 
-plot.bgvecest <- function(x, ci = 0.95, type = "hist", model = NULL, ...) {
+plot.bgvecest <- function(x, ci = 0.95, type = "hist", ctry = NULL, ...) {
   
-  if (is.null(model)) {
-    
-    for (i in 1:length(x)) {
-      plot(x[[i]], ci = ci, type = type, ctry = names(x)[i], ...)
-    }
-    
-  } else {
-    
-    if ("character" %in% class(model)) {
-      model <- which(names(x) %in% model)
-      if (length(model) == 0) {
-        stop("There is no output for the specified country.")
-      }
-    }
-    
-    if (any(class(model) %in% c("numeric", "integer"))) {
-      for (i in model) {
-        plot(x[[i]], ci = ci, type = type, ctry = names(x)[i], ...)
-      }
-    }
+  pos <- 1:length(x)
+  if (!is.null(ctry)) {
+    pos <- which(names(x) %in% ctry)
+    if (length(pos) == 0) {
+      stop("There is no output for the specified country.")
+    } 
   }
+  
+  for (i in pos) {
+    plot(x[[i]], ci = ci, type = type, ctry = names(x)[i], ...)
+  }
+  
 }
 
 
