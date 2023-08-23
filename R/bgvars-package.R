a96e6c235ca1337dd25009c3a09fa99e9b49c344
Franz Mohr <franz.x.mohr@outlook.com>
2023-08-23 20:14:02 +0200

    Add ORCID ID

---
+++ R/bgvars-package.R
@@ -0,0 +1,9 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @useDynLib bgvars, .registration = TRUE
+#' @importFrom Rcpp sourceCpp
+#' @exportPattern "^[[:alpha:]]+"
+## usethis namespace: end
+NULL
