daebf776309542a540e990a3537b442aca1eb77d
Franz Mohr <franz.x.mohr@outlook.com>
2023-08-26 13:01:47 +0200

    Drastic measure

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -5,18 +5,6 @@
     .Call(`_bgvars_bgvaralg`, object)
 }
 
-.bgvartvpalg <- function(object) {
-    .Call(`_bgvars_bgvartvpalg`, object)
-}
-
-.bgvecalg <- function(object) {
-    .Call(`_bgvars_bgvecalg`, object)
-}
-
-.bgvectvpalg <- function(object) {
-    .Call(`_bgvars_bgvectvpalg`, object)
-}
-
 .draw_forecast <- function(i, k, a0, a, b_, c_, sigma, pred) {
     .Call(`_bgvars_draw_forecast`, i, k, a0, a, b_, c_, sigma, pred)
 }
